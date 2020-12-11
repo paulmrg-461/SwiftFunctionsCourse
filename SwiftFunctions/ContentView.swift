528568eb2317b16471d4a293a4cf529dcacba8e8
Paul Realpe <paulmrg461@gmail.com>
2020-12-11 09:20:52 -0500

    Initial Commit

---
+++ SwiftFunctions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftFunctions
+//
+//  Created by Paul Realpe on 11/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftFunctions/SwiftFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFunctionsApp.swift
+//  SwiftFunctions
+//
+//  Created by Paul Realpe on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
