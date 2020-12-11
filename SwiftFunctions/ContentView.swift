bdac1f19cf5dbfde1a5db9f001620ec24a269f6d
Paul Realpe <paulmrg461@gmail.com>
2020-12-11 10:23:09 -0500

    3.InputParameters

--- SwiftFunctions/ContentView.swift
+++ SwiftFunctions/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(greeting(person: "Paul Realpe"))
+        Text(greeting(person: "Paul Realpe", age: 24, isMale: true))
             .padding()
     }
 }
@@ -19,8 +19,33 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-func greeting(person: String) -> String {
-    let greet = "Hola, \(person)!"
+
+func greeting(person: String, age: Int, isMale: Bool) -> String {
+    let greet = isMale ? "\(getMenGreetingMessage(age: age)) \(person)"  : "\(getWomenGreetingMessage(age: age)) \(person)"
     return greet
 }
+//Saludar mujer
+func getWomenGreetingMessage(age: Int) -> String {
+    switch age {
+    case 12...18:
+        return "Bienvenida señorita"
+    case 19...90:
+        return "Bienvenida señora"
+    default:
+        return "Bienvenida"
+    }
+}
+//Saludar hombre
+func getMenGreetingMessage(age: Int) -> String {
+    switch age {
+    case 12...18:
+        return "Bienvenido joven"
+    case 19...90:
+        return "Bienvenido señor"
+    default:
+        return "Bienvenido"
+    }
+}
+
+
 
